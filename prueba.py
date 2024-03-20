ba636dbf4332dfc8b916faff7c1588aee5e45ae5
TIMBIANO <ma.timbiano@uea.edu.ec>
2024-03-20 11:17:21 -0500

    Tareas_UEA

---
+++ prueba.py
@@ -0,0 +1,19 @@
+#Operaciones_matematicas
+matriz = [
+    [13, 15, 17],
+    [21, 39, 25],
+    [31, 35, 37]
+]
+# Definir_valor
+valor = 56
+# Realizar_la_busqueda
+encontrado = False
+for fila in matriz:
+    for elemento in fila:
+        if elemento == valor:
+            encontrado = True
+# Mostrar_resultado
+if encontrado:
+    print("El valor se encontró en la matriz")
+else:
+    print("El valor no se encontró")
